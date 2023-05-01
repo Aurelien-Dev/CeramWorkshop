7b99ea156e4714752b88f6dd539c51f38e72c86f
Aurelien <a.cretaine@live.fr>
2023-05-01 11:45:58 +0200

    Rollback API rout for populate random data

--- Sources/Web/Client/Controllers/FileServiceController.cs
+++ Sources/Web/Client/Controllers/FileServiceController.cs
@@ -23,55 +23,55 @@ namespace Client.Controllers
             ImgBbService = imgBbService;
         }
 
-        /// <summary>
-        /// Permet de réduire la taille des images en passant par le service imgBB,
-        /// initialement prvu d'être hébergé chez eux directement mais leur API n'est vraiment pas pratique
-        /// </summary>
-        /// <returns>success = truc if OK</returns>
-        [HttpGet]
-        [Route("SynchroImageFile")]
-        public async Task<ActionResult> SynchroImageFile()
-        {
-            try
-            {
-                IEnumerable<ImageInstruction> imagesNotExported = await ApiWorker.ImageInstructionRepository.GetAllNonExported();
-                StringBuilder errors = new StringBuilder();
-
-                foreach (var item in imagesNotExported)
-                {
-                    try
-                    {
-                        string path = Path.Combine(EnvironementSingleton.WebRootPath,  item.Url);
-                        (_, string medium, _) = await ImgBbService.UploadFile(path);
-                        string localMedium = await ImgBbService.DownloadFile(medium, path);
-
-                        string directory = WebPathHelper.GetDirectoryName(item.Url);
-
-                        if (string.IsNullOrEmpty(directory))
-                            throw new InvalidOperationException("Directory path is null or empty.");
-
-                        item.Url = WebPathHelper.Combine(directory, Path.GetFileName(localMedium));
-                        item.FileLocation = Location.ImgBb;
-
-                        ApiWorker.ImageInstructionRepository.Update(item);
-                        await ApiWorker.Completed();
-
-                        LoadFileFromInputFile.RemoveFileInput(path);
-                    }
-                    catch (Exception ex)
-                    {
-                        errors.AppendLine($"Error no file : {item.Url}");
-                        errors.AppendLine(ex.Message);
-                    }
-                }
-
-                return new JsonResult(new { sucess = true, errors = errors.ToString() });
-            }
-            catch (Exception ex)
-            {
-                ApiWorker.Rollback();
-                return new JsonResult(new { sucess = false, error = ex.Message, stacktrace = ex.StackTrace });
-            }
-        }
+        // /// <summary>
+        // /// Permet de réduire la taille des images en passant par le service imgBB,
+        // /// initialement prvu d'être hébergé chez eux directement mais leur API n'est vraiment pas pratique
+        // /// </summary>
+        // /// <returns>success = truc if OK</returns>
+        // [HttpGet]
+        // [Route("SynchroImageFile")]
+        // public async Task<ActionResult> SynchroImageFile()
+        // {
+        //     try
+        //     {
+        //         IEnumerable<ImageInstruction> imagesNotExported = await ApiWorker.ImageInstructionRepository.GetAllNonExported();
+        //         StringBuilder errors = new StringBuilder();
+        //
+        //         foreach (var item in imagesNotExported)
+        //         {
+        //             try
+        //             {
+        //                 string path = Path.Combine(EnvironementSingleton.WebRootPath,  item.Url);
+        //                 (_, string medium, _) = await ImgBbService.UploadFile(path);
+        //                 string localMedium = await ImgBbService.DownloadFile(medium, path);
+        //
+        //                 string directory = WebPathHelper.GetDirectoryName(item.Url);
+        //
+        //                 if (string.IsNullOrEmpty(directory))
+        //                     throw new InvalidOperationException("Directory path is null or empty.");
+        //
+        //                 item.Url = WebPathHelper.Combine(directory, Path.GetFileName(localMedium));
+        //                 item.FileLocation = Location.ImgBb;
+        //
+        //                 ApiWorker.ImageInstructionRepository.Update(item);
+        //                 await ApiWorker.Completed();
+        //
+        //                 LoadFileFromInputFile.RemoveFileInput(path);
+        //             }
+        //             catch (Exception ex)
+        //             {
+        //                 errors.AppendLine($"Error no file : {item.Url}");
+        //                 errors.AppendLine(ex.Message);
+        //             }
+        //         }
+        //
+        //         return new JsonResult(new { sucess = true, errors = errors.ToString() });
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         ApiWorker.Rollback();
+        //         return new JsonResult(new { sucess = false, error = ex.Message, stacktrace = ex.StackTrace });
+        //     }
+        // }
     }
 }
